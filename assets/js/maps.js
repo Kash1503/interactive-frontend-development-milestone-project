ae75f07d838adcebd1057c08a9c20bc04d86ceda
Kashif Javed <kash.animations@gmail.com>
2019-02-17 14:04:43 +0000

    Add basic search function with autocomplete to maps.js. Fix some styling bugs from switch to scss.

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -3,4 +3,56 @@ function initMap() {
         center: { lat: 0, lng: 0 },
         zoom: 2
     });
-}
+
+    var input = document.getElementById('search-bar');
+    var searchBox = new google.maps.places.SearchBox(input);
+    map.controls.push(input);
+
+    map.addListener('bounds_changed', function() {
+        searchBox.setBounds(map.getBounds());
+    });
+
+    var markers = [];
+
+    searchBox.addListener('places_changed', function() {
+            var places = searchBox.getPlaces();
+
+            if (places.length == 0) {
+                return;
+            }
+        
+        markers.forEach(function(marker) {
+            marker.setMap(null);
+          });
+          markers = [];
+          
+          var bounds = new google.maps.LatLngBounds();
+          places.forEach(function(place) {
+            if (!place.geometry) {
+              console.log("Returned place contains no geometry");
+              return;
+            }
+            var icon = {
+              url: place.icon,
+              size: new google.maps.Size(71, 71),
+              origin: new google.maps.Point(0, 0),
+              anchor: new google.maps.Point(17, 34),
+              scaledSize: new google.maps.Size(25, 25)
+            };
+            
+            markers.push(new google.maps.Marker({
+              map: map,
+              icon: icon,
+              title: place.name,
+              position: place.geometry.location
+            }));
+            
+            if (place.geometry.viewport) {
+              bounds.union(place.geometry.viewport);
+            } else {
+              bounds.extend(place.geometry.location);
+            }
+          });
+          map.fitBounds(bounds);
+        });
+      }
