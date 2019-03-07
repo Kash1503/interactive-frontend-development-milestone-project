0959dd00675f5468a260b5c1c35a700cc7642cdd
Kashif Javed <kash.animations@gmail.com>
2019-03-07 15:30:19 +0000

    make the noSubmit function jasmine test clearer in its description

--- assets/spec/no-submitSpec.js
+++ assets/spec/no-submitSpec.js
@@ -1,17 +1,17 @@
 describe ('noSubmit Tests', function(){
-   it ("should return 'false'", function(){
+   it ("when 'return' key is pressed, should return 'false'", function(){
        expect(noSubmit(13)).toBe(false);
    }); 
-   it ("should return 'true'", function(){
+   it ("when any key other than 'return' is pressed, should return 'true'", function(){
       expect(noSubmit(1)).toBe(undefined); 
    });
-   it ("should return 'true'", function(){
+   it ("when any key other than 'return' is pressed, should return 'true'", function(){
       expect(noSubmit(5)).toBe(undefined); 
    });
-   it ("should return 'true'", function(){
+   it ("when any key other than 'return' is pressed, should return 'true'", function(){
       expect(noSubmit(17)).toBe(undefined); 
    });
-   it ("should return 'true'", function(){
+   it ("when any key other than 'return' is pressed, should return 'true'", function(){
       expect(noSubmit(24)).toBe(undefined); 
    });
 });
